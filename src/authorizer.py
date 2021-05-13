63f20d30faeb557d9cf7a2f5a77487b8e1f36712
flavio <flavio.assis@picpay.com>
2021-05-13 12:01:16 -0300

    ongoing solution day6

--- src/authorizer.py
+++ src/authorizer.py
@@ -1,25 +1,24 @@
 import json
 from sys import argv
 
-from src.models.context import Context
+from src.models import Context
 from src.utils.event import event_reader
 from src.utils.logger import logger
 from src.validators import get_violations
-from src.utils.find_account import find_account
 
 
-def authorizer(json_file_path):
-    context_list = []
+def authorizer(json_file_path: str) -> None:
+    """
+    Authorizer function
+    :param json_file_path: path of the input file
+    """
+    context = Context()
 
     file_lines = [line.strip() for line in open(json_file_path)]
 
     for line in file_lines:
         json_input = json.loads(line)
         event_type, event = event_reader(json_input)
-
-        account_id = event.id if event_type == 'account' else event.account_id
-
-        context = find_account(account_id, context_list)
 
         violations = get_violations(event_type=event_type, event=event, context=context)
 
@@ -30,15 +29,16 @@ def authorizer(json_file_path):
                 context.account.withdraw(event.amount)
                 context.transactions.append(event)
 
-            context_list.append(context)
-
         info_account: dict = {'account': json.loads(f'{context.account}'), 'violations': violations}
         logger.info(json.dumps(info_account))
 
 
 def main():
-    json_file_path = argv[1]
-    authorizer(json_file_path)
+    try:
+        json_file_path = argv[1]
+        authorizer(json_file_path)
+    except Exception as err:
+        logger.error(f'An error occurred reading the input file: {err}')
 
 
 if __name__ == '__main__':
