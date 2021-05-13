e5373ee95288b4a7235874daab08bdec45e9962e
flavio <flavio.assis@picpay.com>
2021-05-13 11:04:51 -0300

    ongoing solution day6

--- tests/unit/models/test_account_model.py
+++ tests/unit/models/test_account_model.py
@@ -5,13 +5,14 @@ from src.models.account import Account
 
 class TestAccountModel(TestCase):
     def setUp(self) -> None:
+        self.id = 1
         self.active_card = True
         self.available_limit = 100
 
     def test_repr_account_initialized_with_params(self) -> None:
-        account = Account(active_card=self.active_card, available_limit=self.available_limit)
+        account = Account(id=self.id, active_card=self.active_card, available_limit=self.available_limit)
 
-        self.assertEqual(f'{account}', '{"active-card": true, "available-limit": 100}')
+        self.assertEqual(f'{account}', '{"id": 1, "active-card": true, "available-limit": 100}')
 
     def test_repr_account_initialized_without_paramsI(self) -> None:
         account = Account()
